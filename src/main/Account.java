7b066deab69dac9da92aa9a7e5c7173063024dd5
ijuhom <2014brand@naver.com>
2019-10-07 23:03:45 +0900

    입/출금 테스트 케이스 작성 과 통과

--- src/main/Account.java
+++ src/main/Account.java
@@ -2,13 +2,22 @@ package main;
 
 public class Account {
 	private int balance;
-	//파라미터 변경
+
 	public Account(int money) {
 		this.balance = money;
 	}
 
 	public int getBalance() {
 		return this.balance;
+	}
+
+	public void deposit(int i) {
+		this.balance += i;
+		
+	}
+
+	public void withdraw(int i) {
+		this.balance -= i;
 	}
 
 }
--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -14,7 +14,14 @@ import main.Account;
  * -계좌 잔고 조회
  * 	1) 10000원으로 계좌 생성
  *  2) 잔고 조회 결과 일치
- * -입금/출금
+ * -입금
+ * 	1) 10000원으로 계좌 생성
+ *  2) 1000원 입금
+ *  3) 잔고 11000원 확인
+ * -출금
+ *  1) 10000원으로 계좌생성
+ *  2) 1000원 출금
+ *  3) 잔고 9000원 확인
  * -예상 복리 이자 (추가 발생)
  * -금액은 원단위 
  * 질문 > 응답 > 정제 의  반복
@@ -31,9 +38,8 @@ public class AccountTest {
 	@Test
 	//잔고 조회 서비스 
 	public void testGetBalance() throws Exception {
-		//1) 10000원으로 계좌 생성 (실패)
+		
 		Account account = new Account(10000);
-		//if 변경
 		assertEquals(10000,account.getBalance());
 		
 		//계속되는 테스트 케이스
@@ -43,5 +49,22 @@ public class AccountTest {
 		//계속되는 테스트 케이스
 		account = new Account(0);
 		assertEquals(0,account.getBalance());
+	}
+	
+	@Test
+	//입금 서비스 (메서드명과 역할을 정해서 테스트케이스 구현)
+	public void testDeposit() throws Exception {
+		Account account = new Account(10000);
+		account.deposit(1000);
+		//실패(결과값 확인후 성공으로 로직구현)
+		assertEquals(11000,account.getBalance());
+		
+	}
+	@Test
+	//출금 서비스
+	public void testWithdraw() throws Exception {
+		Account account = new Account(10000);
+		account.withdraw(1000);
+		assertEquals(9000,account.getBalance());
 	}
 }
