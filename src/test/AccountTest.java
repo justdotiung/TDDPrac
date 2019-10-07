b1e73595df1aad54289bc194bfa11a7fb8ebcb02
ijuhom <2014brand@naver.com>
2019-10-07 22:39:34 +0900

    잔고 조회 test case 및 main 리팩토링

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -26,29 +26,22 @@ public class AccountTest {
 	@Test
 	//Account 계좌 생성 클래스
 	public void testAccount() throws Exception {
-		//테스트로 인해 무조건 적인 기본생성자를 추가하는것이 아닌 업무적으로 생각하게끔 만들어줄 수 도 있다 
-		//ex) 예치금이 있이 계좌생성 or 없어도 계좌생성 가능 
-		Account account = new Account(10000); //오류발생 해결.
+		Account account = new Account(10000);
 	}
 	@Test
 	//잔고 조회 서비스 
-	// getBalacne() 잔고조회 메서드 
 	public void testGetBalance() throws Exception {
 		//1) 10000원으로 계좌 생성 (실패)
 		Account account = new Account(10000);
-		//2) 잔고 조회 결과 일치 (실패)
-		if(account.getBalance() != 10000) {
-			//throw new Exception(); 오류
-			fail("getBalance() > "+account.getBalance()); //실패
-		}
+		//if 변경
+		assertEquals(10000,account.getBalance());
+		
 		//계속되는 테스트 케이스
 		account = new Account(1000);
-		if(account.getBalance() != 1000)
-			fail();
+		assertEquals("1000원으로 계좌 생성후 잔고조회 ",1000,account.getBalance());
 		
 		//계속되는 테스트 케이스
 		account = new Account(0);
-		if(account.getBalance() != 0)
-			fail();
+		assertEquals(0,account.getBalance());
 	}
 }
